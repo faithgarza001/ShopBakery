c0904532fe36d3d0ec118719868278f070ed8011
faithgarza001 <faith.garza001@gmail.com>
2022-11-22 10:31:59 -0600

    first commit

---
+++ DemoShopCafe/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DemoShopCafe.Models
+{
+	public class Category
+	{
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string? Description { get; set; }
+
+        public List<Pie>? Pies { get; set; }
+    }
+}
+
