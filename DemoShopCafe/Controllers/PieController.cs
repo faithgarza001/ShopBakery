0081894c1f5733289a3c895c749e4ca4b59eb151
faithgarza001 <faith.garza001@gmail.com>
2022-11-22 10:31:59 -0600

    first commit

---
+++ DemoShopCafe/Controllers/PieController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoShopCafe.Models;
+using DemoShopCafe.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoShopCafe.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        // GET: /<controller>/
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+            //return View( _pieRepository.AllPies);
+
+            PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+            return View(piesListViewModel);
+
+        }
+    }
+
+}
+
