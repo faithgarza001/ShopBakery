33c3dd57a7c7254d218626e9ae7dbebd64dd87a4
faithgarza001 <faith.garza001@gmail.com>
2022-11-29 08:26:00 -0600

    added db class to begin SQL

---
+++ DemoShopCafe/Models/DemoShopCafeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoShopCafe.Models
+{
+	public class DemoShopCafeDbContext : DbContext
+	{
+		public DemoShopCafeDbContext(DbContextOptions<DemoShopCafeDbContext> options) : base(options)
+		{
+		}
+	}
+}
+
